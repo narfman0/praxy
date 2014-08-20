1cb350bdc8c270e148b5c000edb77b4e5165fc27
Jon Robison <jon.robison@coxinc.com>
2014-08-20 12:15:21 -0400

    -Change to git submodule to update php proxy indepdently of praxy

--- provision.sh
+++ provision.sh
@@ -4,6 +4,7 @@ yum install -y httpd vim php php-common
 rm -rf /var/www/html
 ln -fs /vagrant /var/www/html
 
+cp php-simple-proxy/ba-simple-proxy.php index.php
 sed -i 's/index.html/index.php/g' /etc/httpd/conf/httpd.conf
 chkconfig httpd on
 service httpd start
