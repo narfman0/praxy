a732b22321099ff9160bdc5d200d9e0a669b2558
Jon Robison <jon.robison@coxinc.com>
2014-08-22 15:56:48 -0400

    -Copy remote proxy instead of saving my own

--- provision.sh
+++ provision.sh
@@ -4,7 +4,7 @@ yum install -y httpd vim php php-common
 rm -rf /var/www/html
 ln -fs /vagrant /var/www/html
 
-cp php-simple-proxy/ba-simple-proxy.php index.php
+cp /vagrant/php-simple-proxy/ba-simple-proxy.php /vagrant/index.php
 sed -i 's/index.html/index.php/g' /etc/httpd/conf/httpd.conf
 chkconfig httpd on
 service httpd start
